1455069f933e1a87d77fc3cc5610f0730e4a8f8d
dguilar1994 <andrea@MacBook-Pro-de-Andrea.local>
2017-06-15 22:45:11 -0500

    Corregir el nombre de la migracion que lo tenia malo

--- db/migrate/20170616034334_add_price_per_night_to_rooms.rb
+++ db/migrate/20170616034334_add_price_per_night_to_rooms.rb
@@ -1,5 +1,5 @@
 class AddPricePerNightToRooms < ActiveRecord::Migration[5.0]
   def change
-    add_column :rooms, :Price_per_night, :decimal
+    add_column :rooms, :price_per_night, :decimal
   end
 end
